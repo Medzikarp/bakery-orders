0e949960fe10078aa2b8b9b570c52e3c4bb75775
lukkotol <lukas.kotol@cleverlance.com>
2019-04-14 21:25:16 +0200

    Added simple logging.

--- app/bakery-orders-service/src/main/java/org/bakery/orders/service/impl/DeliveryOrderServiceImpl.java
+++ app/bakery-orders-service/src/main/java/org/bakery/orders/service/impl/DeliveryOrderServiceImpl.java
@@ -5,6 +5,7 @@ import org.bakery.orders.dao.UserDao;
 import org.bakery.orders.entity.DeliveryOrder;
 import org.bakery.orders.entity.User;
 import org.bakery.orders.service.DeliveryOrderService;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -18,6 +19,8 @@ import java.util.List;
 @ApplicationScoped
 public class DeliveryOrderServiceImpl implements DeliveryOrderService {
 
+    private static final Logger LOGGER = Logger.getLogger(DeliveryOrderServiceImpl.class.getName());
+
     @Inject
     private DeliveryOrderDao deliveryOrderDao;
 
@@ -27,42 +30,50 @@ public class DeliveryOrderServiceImpl implements DeliveryOrderService {
     @Override
     public DeliveryOrder create(DeliveryOrder deliveryOrder) {
         DeliveryOrder created = deliveryOrderDao.create(deliveryOrder);
+        LOGGER.info("DeliveryOrder created with id " + deliveryOrder.getId());
         return created;
     }
 
     @Override
     public DeliveryOrder update(DeliveryOrder deliveryOrder) {
+        LOGGER.info("Updating DeliveryOrder with id " + deliveryOrder.getId());
         DeliveryOrder updated = deliveryOrderDao.update(deliveryOrder);
         return updated;
     }
 
     @Override
     public void remove(DeliveryOrder deliveryOrder) {
+        LOGGER.info("Removing DeliveryOrder with id " + deliveryOrder.getId());
         deliveryOrderDao.remove(deliveryOrder.getId());
     }
 
     @Override
     public DeliveryOrder findById(Long id) {
+        LOGGER.info("Searching for DeliveryOrder with id " + id);
         return deliveryOrderDao.find(id);
     }
 
     @Override
     public List<DeliveryOrder> findAll() {
+        LOGGER.info("Searching for all DeliveryOrders");
         return deliveryOrderDao.findAll();
     }
 
     @Override
     public void removeAll() {
+        LOGGER.info("Removing all DeliveryOrders");
         deliveryOrderDao.removeAll();
     }
 
     @Override
     public List<DeliveryOrder> searchByUser(User user) {
+        LOGGER.info("Searching for all DeliveryOrders with user id " + user.getId());
         return deliveryOrderDao.searchByUser(user);
     }
 
     @Override
     public void addUserToDeliveryOrder(@NotNull Long deliveryOrderId, @NotNull Long userId) {
+        LOGGER.info("Adding user " + userId + " to DeliveryOrder " + deliveryOrderId);
         DeliveryOrder deliveryOrder = findById(deliveryOrderId);
         User user = userDao.find(userId);
         deliveryOrder.setUser(user);
--- app/bakery-orders-service/src/main/java/org/bakery/orders/service/impl/UserServiceImpl.java
+++ app/bakery-orders-service/src/main/java/org/bakery/orders/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.bakery.orders.dao.DeliveryOrderDao;
 import org.bakery.orders.dao.UserDao;
 import org.bakery.orders.entity.User;
 import org.bakery.orders.service.UserService;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -16,6 +17,9 @@ import java.util.List;
 @ApplicationScoped
 public class UserServiceImpl implements UserService {
 
+    private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class.getName());
+
+
     @Inject
     private UserDao userDao;
 
@@ -25,33 +29,39 @@ public class UserServiceImpl implements UserService {
     @Override
     public User create(User user) {
         User created = userDao.create(user);
+        LOGGER.info("User created with id " + user.getId());
         return created;
     }
 
     @Override
     public User update(User user) {
+        LOGGER.info("Updating User with id " + user.getId());
         User updated = userDao.update(user);
         return updated;
     }
 
     @Override
     public void remove(User user) {
+        LOGGER.info("Removing User with id " + user.getId());
         deliveryOrderDao.searchByUser(user).forEach(deliveryOrder -> deliveryOrder.setUser(null));
         userDao.remove(user.getId());
     }
 
     @Override
     public User findById(Long id) {
+        LOGGER.info("Searching for User with id " + id);
         return userDao.find(id);
     }
 
     @Override
     public List<User> findAll() {
+        LOGGER.info("Searching for all Users");
         return userDao.findAll();
     }
 
     @Override
     public void removeAll() {
+        LOGGER.info("Removing all Users");
         userDao.removeAll();
     }
 }

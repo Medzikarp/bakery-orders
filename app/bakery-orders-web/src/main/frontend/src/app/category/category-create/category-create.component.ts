b1848957b94558e082583d596d342d043b846ded
lukkotol <lukas.kotol@cleverlance.com>
2019-04-30 19:58:21 +0200

    CSS table changed.

--- app/bakery-orders-web/src/main/frontend/src/app/category/category-create/category-create.component.ts
+++ app/bakery-orders-web/src/main/frontend/src/app/category/category-create/category-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CategoryService} from "../../services/category.service";
 import {ProductService} from "../../services/product.service";
@@ -7,41 +7,40 @@ import {Product} from "../../model/product";
 import {Category} from "../../model/category";
 
 @Component({
-  selector: 'app-category-create',
-  templateUrl: './category-create.component.html',
-  styleUrls: ['./category-create.component.css']
+    selector: 'app-category-create',
+    templateUrl: './category-create.component.html',
+    styleUrls: ['./category-create.component.css']
 })
 export class CategoryCreateComponent {
 
-  createCategory: FormGroup;
+    createCategory: FormGroup;
 
-  constructor(private categoryService: CategoryService, private productService: ProductService, private fb: FormBuilder, private router: Router) {
-    this.createCategory = fb.group({
-      'name': [null, Validators.required],
-      'description': [null]
-    });
-  }
+    constructor(private categoryService: CategoryService, private productService: ProductService, private fb: FormBuilder, private router: Router) {
+        this.createCategory = fb.group({
+            'name': [null, Validators.required],
+            'description': [null]
+        });
+    }
 
-  onSubmit() {
-    let category = new Category();
-    category.name = this.createCategory.get('name').value;
-    category.description = this.createCategory.get('description').value;
+    onSubmit() {
+        let category = new Category();
+        category.name = this.createCategory.get('name').value;
+        category.description = this.createCategory.get('description').value;
 
 
+        this.categoryService.createCategory(category).subscribe(
+            data => {
+                console.log(data);
+            },
+            err => console.error(err),
+            () => this.router.navigateByUrl('/category')
+        );
 
-    this.categoryService.createCategory(category).subscribe(
-      data => {
-        console.log(data);
-      },
-      err => console.error(err),
-      () => this.router.navigateByUrl('/category')
-    );
+    }
 
-  }
-
-  getNameErrorMessage() {
-    return this.createCategory.get('name').hasError('required') ? 'You must enter a value' :
-      '';
-  }
+    getNameErrorMessage() {
+        return this.createCategory.get('name').hasError('required') ? 'You must enter a value' :
+            '';
+    }
 
 }
